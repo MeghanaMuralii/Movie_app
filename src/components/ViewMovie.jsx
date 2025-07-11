37d167da3707ca73d7c8dcf7a1f5ad2a68d11e45
MeghanaMurali <meghanaminnu1234@gmail.com>
2025-07-11 12:00:21 +0530

    Completed

---
+++ src/components/ViewMovie.jsx
@@ -0,0 +1,122 @@
+import React from 'react'
+import MovieNavbar from './MovieNavbar'
+
+const ViewMovie = () => {
+    return (
+        <div>
+            <MovieNavbar />
+            <div className="container">
+                <div className="row">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <div className="row g-3">
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                <div class="card" >
+                                    
+                                    <img src="https://qqcdnpictest.mxplay.com/pic/9c644f2c28410f9321fd9b572cf17e85/en/2x3/312x468/f74ae9251666f8737e32e6a335ffef6d_1280x1920.webp" class="card-img-top" alt="..."/>
+                                        <div class="card-body">
+                                            <h5 class="card-title">Sita Ramam</h5>
+                                            
+                                            <a href="#" class="btn btn-primary">WATCH MOVIE</a>
+                                        </div>
+                                </div>
+
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                <div class="card" >
+                                    <img src="https://storage.googleapis.com/dev-cms-image-test/movie/2/1065772/img145210/6923404/1065772_6923404.jpg" class="card-img-top" alt="..."/>
+                                        <div class="card-body">
+                                            <h5 class="card-title">Boby</h5>
+                                            
+                                            <a href="#" class="btn btn-primary">WATCH MOVIE</a>
+                                        </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                <div class="card" >
+                                    <img src="https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC/et00451621-tjvydasxpl-portrait.jpg" class="card-img-top" alt="..."/>
+                                        <div class="card-body">
+                                            <h5 class="card-title">Dheeran</h5>
+                                            
+                                            <a href="#" class="btn btn-primary">WATCH MOVIE</a>
+                                        </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                <div class="card" >
+                                    <img src="https://images.indianexpress.com/2023/12/romancham-24122023.jpg" class="card-img-top" alt="..."/>
+                                        <div class="card-body">
+                                            <h5 class="card-title">Romanjam</h5>
+                                            
+                                            <a href="#" class="btn btn-primary">WATCH MOVIE</a>
+                                        </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                <div class="card" >
+                                    <img src="https://cf-img-a-in.tosshub.com/sites/visualstory/wp/2025/06/maluITG-1749099485284.jpg?size=*:900" class="card-img-top" alt="..."/>
+                                        <div class="card-body">
+                                            <h5 class="card-title">Thudarum</h5>
+                                            
+                                            <a href="#" class="btn btn-primary">WATCH MOVIE</a>
+                                        </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                <div class="card" >
+                                    <img src="https://akamaividz2.zee5.com/image/upload/w_336,h_504,c_scale,f_webp,q_auto:eco/resources/0-0-1z5769171/portrait/1920x77021a1adc4e6fe423282f3e64aee87e6cf0d15a24400b341b1ae1944de81e7a69c.jpg" class="card-img-top" alt="..."/>
+                                        <div class="card-body">
+                                            <h5 class="card-title">Eleven</h5>
+                                            
+                                            <a href="#" class="btn btn-primary">WATCH MOVIE</a>
+                                        </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                <div class="card" >
+                                    <img src="https://images.timesnownews.com/thumb/msid-104744561,thumbsize-62918,width-1280,height-720,resizemode-75/104744561.jpg" class="card-img-top" alt="..."/>
+                                        <div class="card-body">
+                                            <h5 class="card-title">Tholvi</h5>
+                                            
+                                            <a href="#" class="btn btn-primary">WATCH MOVIE</a>
+                                        </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                <div class="card" >
+                                    <img src="https://i0.wp.com/ficklesorts.com/wp-content/uploads/2023/10/home-film.jpg?fit=784%2C543&ssl=1" class="card-img-top" alt="..."/>
+                                        <div class="card-body">
+                                            <h5 class="card-title">Home</h5>
+                                            
+                                            <a href="#" class="btn btn-primary">WATCH MOVIE</a>
+                                        </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                <div class="card" >
+                                    <img src="https://images.slivcdn.com/videoasset_images/toby_20dec_malayalam_portrait_thumb.jpg?w=360&q=low" class="card-img-top" alt="..."/>
+                                        <div class="card-body">
+                                            <h5 class="card-title">Toby</h5>
+                                            
+                                            <a href="#" class="btn btn-primary">WATCH MOVIE</a>
+                                        </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                <div class="card" >
+                                    <img src="https://m.media-amazon.com/images/M/MV5BMjBjYzVmOTAtMWI2NC00ODhiLTk4NDYtYmY3YmQxYmU2NTI5XkEyXkFqcGc@._V1_.jpg" class="card-img-top" alt="..."/>
+                                        <div class="card-body">
+                                            <h5 class="card-title">2018</h5>
+                                            
+                                            <a href="#" class="btn btn-primary">WATCH MOVIE</a>
+                                        </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ViewMovie
